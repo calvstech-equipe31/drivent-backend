f74057eaf40512ab4cc4e3c1351448c5dc02decc
Bismarck <bismarckdados@gmail.com>
2023-03-13 14:42:42 -0300

    feat: implementing redis in events.

--- src/controllers/events-controller.ts
+++ src/controllers/events-controller.ts
@@ -1,11 +1,26 @@
-import eventsService from "@/services/events-service";
-import { Request, Response } from "express";
-import httpStatus from "http-status";
+import { createClient } from 'redis';
+import eventsService from '@/services/events-service';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
 
 export async function getDefaultEvent(_req: Request, res: Response) {
+  const redis = createClient({
+    url: process.env.REDIS_URL,
+  });
+  await redis.connect();
+
+  const cacheKey = 'eventConfig';
+
   try {
-    const event = await eventsService.getFirstEvent();
-    return res.status(httpStatus.OK).send(event);
+    const cachedEvent = await redis.get(cacheKey);
+    console.log(cachedEvent);
+    if (cachedEvent) {
+      return res.status(httpStatus.OK).send(JSON.parse(cachedEvent));
+    } else {
+      const data = await eventsService.getFirstEvent();
+      redis.set(cacheKey, JSON.stringify(data));
+      return res.status(httpStatus.OK).send(data);
+    }
   } catch (error) {
     return res.status(httpStatus.NOT_FOUND).send({});
   }
