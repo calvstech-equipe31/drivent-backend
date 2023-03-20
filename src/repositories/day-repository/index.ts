28e6dbcc8c917d7cc4c045eed73592a912ee2dae
nandasino <fernandamsino@gmail.com>
2023-03-20 12:07:48 -0300

    seed de dias

--- src/repositories/day-repository/index.ts
+++ src/repositories/day-repository/index.ts
@@ -1,11 +1,12 @@
 import { prisma } from "@/config";
 
 async function findDays() {
-  //return prisma.days.findMany();
-  return [
+  return prisma.day.findMany();
+  /*return [
     { id: 1, name: "Segunda", date: "20/03" },
     { id: 2, name: "Terça", date: "21/03" },
-  ];
+    { id:3, name: "Quarta, date: "22/03" }
+  ];*/
 }
 
 const dayRepository = {
